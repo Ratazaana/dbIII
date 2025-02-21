5e446ebf115a684935dfa0e2e92ca944fe99142a
Ratazaana <gustavo.martins174@etec.sp.gov.br>
2025-02-21 19:38:15 -0300

    Add files via upload

---
+++ DBTDS3H_2025/dbpadaria.sql
@@ -0,0 +1,84 @@
+-- apagando banco de dados se existir
+drop database dbpadaria;
+-- criando banco de dados 
+create database dbpadaria;
+-- acessar o banco de dados
+use dbpadaria;
+--criando as tabelas no banco de dados
+
+create table tbGenero(
+codGen int not null auto_increment,
+nome varchar(100),
+primary key(codGen));
+
+create table tbFuncionarios(
+codFunc int not null auto_increment,
+nome varchar(100),
+email varchar(100),
+telCel char(10),
+dataNasc datetime,
+salario decimal(9,2) default 0 check(salario >= 0),
+sexo char(1) default "F" check(sexo in("F","M")),
+codGen int not null,
+primary key(codFunc),
+foreign key(codGen)references tbGenero(codGen));
+
+create table tbUsuarios(
+codUsu int not null auto_increment,
+nome varchar(50) not null,
+senha varchar(12) not null,
+codFunc int not null,
+primary key(codUsu),
+foreign key(codFunc)references tbFuncionarios(codFunc)
+);
+
+create table tbFornecedores(
+codForn int not null auto_increment,
+nome varchar(100),
+email varchar(100),
+telCel char(10),
+primary key(codForn));
+
+create table tbProdutos(
+codProd int not null auto_increment,
+descricao varchar(100),
+quantidade int,
+preco decimal(9,2),
+dataVal datetime,
+lote char(10),
+codForn int not null,
+primary key(codProd),
+foreign key(codForn)references tbFornecedores(codForn));
+
+create table tbClientes(
+codCli int NOT NULL AUTO_INCREMENT,
+nome varchar(100),
+cpf char(14) NOT NULL unique,
+email varchar(100),
+primary key(codCli)
+);
+
+create table tbVendas(
+codVend int not null auto_increment,
+dataVend date, 
+horaVend time,
+quantidade decimal (9,2) default 0 check(quantidade >=0),
+valor decimal (9,2) default 0 check (valor >=0),
+codProd int not null, 
+codUsu INT NOT NULL,
+codCli int NOT NULL,
+primary key (codVend),
+foreign key (codProd) references tbProdutos(codProd),
+foreign key (codUsu) references tbUsuarios(codUsu),
+foreign key (codCli) references tbClientes(codCli));
+
+-- visualizando as tabelas
+show tables;
+-- visualizando a estrutura das tabelas
+desc tbGenero;
+desc tbUsuarios;
+desc tbFornecedores;
+desc tbFuncionarios;
+desc tbClientes;
+desc tbVendas;
+desc tbProdutos;
